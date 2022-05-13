40fa5f93ae166e335955e9e37f8d51a244f7e7a5
sanghyupjang <sanghyeob93@naver.com>
2022-05-13 15:13:15 +0900

    push

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -6,11 +6,13 @@ import {
   Delete,
   Patch,
   Body,
-  Query,
+  Req,
+  Res,
 } from '@nestjs/common';
 import { MoviesService } from './movies.service';
 import { Movie } from './entities/movie.entity';
 import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
 
 @Controller('movies')
 export class MoviesController {
@@ -37,7 +39,7 @@ export class MoviesController {
   }
 
   @Patch(':id')
-  patch(@Param('id') movieId: number, @Body() updateData) {
+  patch(@Param('id') movieId: number, @Body() updateData: UpdateMovieDto) {
     return this.moviesService.update(movieId, updateData);
   }
 }
