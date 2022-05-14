154cce2f200f892b726d9a523224d3bd8bd427e9
sanghyupjang <sanghyeob93@naver.com>
2022-05-14 10:49:42 +0900

    push

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,102 +1,90 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { MoviesService } from './movies.service';
-import { NotFoundException } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from './../src/app.module';
 
-describe('MoviesService', () => {
-  let service: MoviesService;
+describe('AppController (e2e)', () => {
+  let app: INestApplication;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [MoviesService],
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
     }).compile();
 
-    service = module.get<MoviesService>(MoviesService);
+    app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    );
+    await app.init();
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('/ (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/')
+      .expect(200)
+      .expect('Welcome to my Movie API');
   });
 
-  describe('getAll', () => {
-    it('should return an array', () => {
-      const result = service.getAll();
-      expect(result).toBeInstanceOf(Array);
+  describe('/movies', () => {
+    it('GET', () => {
+      return request(app.getHttpServer())
+        .get('/movies')
+        .expect(200)
+        .expect([]);
     });
-  });
-
-  describe('getOne', () => {
-    it('should return a movie', () => {
-      service.create({
-        title: 'Test Movie',
-        genres: ['test'],
-        year: 2000,
-      });
-      const movie = service.getOne(1);
-      expect(movie).toBeDefined();
+    it('POST 201', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: 'Test',
+          year: 2000,
+          genres: ['test'],
+        })
+        .expect(201);
     });
-
-    it('should throw 404 error', () => {
-      try {
-        service.getOne(999);
-      } catch (e) {
-        expect(e).toBeInstanceOf(NotFoundException);
-      }
+    it('POST 400', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: 'Test',
+          year: 2000,
+          genres: ['test'],
+          other: 'thing',
+        })
+        .expect(400);
     });
-  });
-
-  describe('deleteOne', () => {
-    it('deletes a movie', () => {
-      service.create({
-        title: 'Test Movie',
-        genres: ['test'],
-        year: 2000,
-      });
-      const beforeDelete = service.getAll().length;
-      service.deleteOne(1);
-      const afterDelete = service.getAll().length;
-      expect(afterDelete).toBeLessThan(beforeDelete);
-    });
-
-    it('should throw a NotFoundException', () => {
-      try {
-        service.deleteOne(999);
-      } catch (e) {
-        expect(e).toBeInstanceOf(NotFoundException);
-      }
+    it('DELETE', () => {
+      return request(app.getHttpServer())
+        .delete('/movies')
+        .expect(404);
     });
   });
 
-  describe('create', () => {
-    it('should create a movie', () => {
-      const beforeCreate = service.getAll().length;
-      service.create({
-        title: 'Test Movie',
-        genres: ['test'],
-        year: 2000,
-      });
-      const afterCreate = service.getAll().length;
-      expect(afterCreate).toBeGreaterThan(beforeCreate);
+  describe('/movies/:id', () => {
+    it('GET 200', () => {
+      return request(app.getHttpServer())
+        .get('/movies/1')
+        .expect(200);
     });
-  });
-
-  describe('update', () => {
-    it('should update a movie', () => {
-      service.create({
-        title: 'Test Movie',
-        genres: ['test'],
-        year: 2000,
-      });
-      service.update(1, { title: 'Updated Test' });
-      const movie = service.getOne(1);
-      expect(movie.title).toEqual('Updated Test');
+    it('GET 404', () => {
+      return request(app.getHttpServer())
+        .get('/movies/999')
+        .expect(404);
     });
-
-    it('should throw a NotFoundException', () => {
-      try {
-        service.update(999, {});
-      } catch (e) {
-        expect(e).toBeInstanceOf(NotFoundException);
-      }
+    it('PATCH 200', () => {
+      return request(app.getHttpServer())
+        .patch('/movies/1')
+        .send({ title: 'Updated Test' })
+        .expect(200);
+    });
+    it('DELETE 200', () => {
+      return request(app.getHttpServer())
+        .delete('/movies/1')
+        .expect(200);
     });
   });
 });
